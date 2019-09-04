a36bd28b1b328b4190f1fc593abcd0588882916e
Egor <egodunaev@gmail.com>
2019-09-04 08:59:56 +0300

    changed scraper, removed city from params

--- scraper.py
+++ scraper.py
@@ -1,38 +1,21 @@
-# from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
-# import os
 from datetime import datetime
 
-# db_address = os.environ.get("DB_ADDRESS", "")
-# db_port = 27017
-# client = MongoClient(db_address, db_port)
-# weather_db = client.weather
 
 
 class GisMeteoScraper:
-    def __init__(self, gismeteo_address, city):
+    def __init__(self, gismeteo_address):
         self.gismeteo_address = gismeteo_address
-        self.city = city
-        # self.check_collection(self.city)
-        self.scrape_info(self.gismeteo_address)
-        self.date_of_scraping = datetime()
+        self.city = self.gismeteo_address.split("-")[1]
+        self.date_of_scraping = None
+        self.weather = dict()
 
-    # def check_collection(self, city):
-    #     try:
-    #         if weather_db[city] is None:
-    #             weather_db.create_collection(city)
-    #             print(f"Collection {city} created")
-    #             return
-    #         print(f"Collection {city} exists")
-    #     except Exception as ex:
-    #         raise ex
-
-    def scrape_info(self, address):
+    def scrape_info(self):
         try:
-            page = requests.get(address, headers={"User-Agent": "Mozilla/5.0"})
+            page = requests.get(self.gismeteo_address, headers={"User-Agent": "Mozilla/5.0"})
             soup = BeautifulSoup(page.content, "html.parser")
-            self.date_of_scraping = str(f"{datetime.today().day()}/{datetime.today().month()}/{datetime.today().year()}")
+            self.date_of_scraping = str(f"{datetime.today().day}/{datetime.today().month}/{datetime.today().year}")
             return soup
         except Exception as ex:
             raise ex
